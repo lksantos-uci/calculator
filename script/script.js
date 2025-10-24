318f2517fc788c5d44ccc43105d850e0f411bbd8
lksantos <lksantos@uci.edu>
2025-10-23 22:08:31 -0700

    Finish basic calculator functionalities and revamped UI
    I finished implementing the basic arithmetic functionalities for the
    calculator such as addition, subtraction, multiplication, and division.
    I implemented helpers for the display, and I also finished the CE button
    which resets the calculator. Lastly, I changed the UI to have a darker
    background and removed the more advanced buttons of the calculator.

--- script/script.js
+++ script/script.js
@@ -1,41 +1,38 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
     if (num2 === 0) alert("You cannot divide by 0"); 
-    else return num1 / num2;
+    else return Number(num1) / Number(num2);
 }
 
 function operate(operator, num1, num2) {
     switch (operator) {
-        case '+':
-            add(num1, num2);
-            break;
-        case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            divide(num1, num2);
-            break;
+        case "addition":
+            return add(num1, num2);
+        case "subtraction":
+            return subtract(num1, num2);
+        case "multiplication":
+            return multiply(num1, num2);
+        case "division":
+            return divide(num1, num2);
     }
 
 }
 
-function displayNumber(num) {
+function displayInput(num) {
     const display = document.querySelector("#num-display");
-    display.textContent += num;
+    if (display.textContent === "0") display.textContent = num;
+    else display.textContent += num;
 }
 
 function clearDisplay() {
@@ -43,15 +40,94 @@ function clearDisplay() {
     display.textContent = "";
 }
 
-function getPressedKey() {
-    const calculator = document.querySelector("#calculator");
-    calculator.addEventListener("click", (event) => {
-        let target = event.target;
-        if (target.className === "number") {
-            const num = target.textContent;
-            displayNumber(num);
-        }
-    });
+function getDisplayContent() {
+    const display = document.querySelector("#num-display");
+    return display.textContent;
 }
 
-getPressedKey();
+function updateDisplay(num) {
+    clearDisplay();
+    displayInput(num);
+}
+
+function resetExpr(expr) {
+    expr.operand1 = null;
+    expr.operand2 = null;
+    expr.operator = null;
+}
+
+function isNumberPressed(target) {
+    return target.className === "num-btn";
+}
+
+function isOperationPressed(target) {
+    return target.className === "op-btn";
+}
+
+function isEqualsPressed(target) {
+    return target.id === "equals";
+}
+
+function isCEPressed(target) {
+    return target.id === "ce-btn";
+
+}
+function isDelPressed(target) {
+    return target.id === "del-btn";
+}
+
+function handleNumbers(target, expr) {
+    const num = target.textContent;
+    if (!expr.operand1) {
+        expr.operand1 = num;
+        updateDisplay(num);
+    }
+    else if (expr.operator && !expr.operand2) { 
+        expr.operand2 = num; 
+        updateDisplay(num);
+    }
+    else displayInput(num);
+}
+
+function handleExpr(expr) {
+    expr.operand2 = getDisplayContent();
+    const result = operate(expr.operator, expr.operand1, expr.operand2);
+    updateDisplay(result);
+    expr.operand1 = expr.operand2 = expr.operator = null;
+}
+
+function handleClick(event, expr) {
+    let target = event.target;
+    if (isNumberPressed(target)) {
+        handleNumbers(target, expr);
+    }
+    else if (isOperationPressed(target) || isEqualsPressed(target)) {
+        if (expr.operand1 && expr.operand2 && expr.operator) {
+            handleExpr(expr);
+        }
+        if (isOperationPressed(target)) {
+            expr.operator = target.id;
+            expr.operand1 = getDisplayContent();
+        }
+    }
+    else if (isCEPressed(target)) {
+        resetExpr(expr);
+        clearDisplay();
+    }
+    else if (isDelPressed(target)) {
+        
+    }
+
+}
+
+function initProgram() {
+    const calculator = document.querySelector("#calculator");
+    let expression = { 
+        operand1: null, 
+        operand2: null, 
+        operator: null
+    };
+    calculator.addEventListener("click", (event) => handleClick(event, expression));
+}
+
+initProgram();
